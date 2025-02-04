1424aefac7a622926efaba5d82f93d5262f04eed
aravinnndddd <aromalaravind0@gmail.com>
2025-02-04 08:22:03 +0530

    Update script.js

--- assets/Script/script.js
+++ assets/Script/script.js
@@ -4,11 +4,7 @@ $("a").click(function(){
    var pageId = $(this).attr("data-page");
   $("html, body").animate({ scrollTop: $("#"+pageId).offset().top }, 1000);
     });
-    function EnterName(){
-      let userName =prompt("Enter your name to see Magic");
-              if (userName != null) {
-                  document.getElementById("name-add").innerHTML = `${userName.charAt(0).toUpperCase()+ userName.slice(1).toLowerCase()}, `;
-  }
+  
     }
 
 // Navbar scroll events
